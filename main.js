fa0691922a21f305ef663f0e71ae4fcaea8ad77b
Nick Martin <nicholas.paul.martin@gmail.com>
2018-11-05 15:20:28 -0800

    Link to data set

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
+//http://tinyurl.com/cs160nov05
 
+const BASE_URL = "https://zagster-service.herokuapp.com"
 
 $(updateView)
 
 function updateView() {
-    alert ("Using jQuery for the first time and sweet pancakes, this is fantastic!")
+    $.getJSON(BASE_URL + "/rides/count", updateRideCount)
 }
+
+function updateRideCount (data) {
+    numberOfRides = data.count
+    $("h2#ridecount").html(numberOfRides)
+}
+//implementing a function - taking action - defining a recipe and giving it a name, you can invoke that recipe////#endregion
+
+//function implementation or definition (above)
+
+//name = identifier
+//code block or steps to follow once the function is executed
+
+//get number of rides....make a request to.../rides/count; grab the value of the count attribute
+//replace todo with the number of rides
